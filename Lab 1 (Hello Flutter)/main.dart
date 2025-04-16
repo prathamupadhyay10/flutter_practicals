b621478549ecaad546e7eb6e75d3a30ddc1ab46c
Jhanvi1711 <jhanvi.thanki116010@marwadiuniversity.ac.in>
2025-04-16 20:21:27 +0530

    Create main.dart

---
+++ Lab 1 (Hello Flutter)/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Text (
+        "Hello Flutter",
+        style: TextStyle (
+            color: Colors.amber,
+fontSize: 30,
+fontWeight: FontWeight.bold
+        ),
+      ),
+    ),
+  );
+}
